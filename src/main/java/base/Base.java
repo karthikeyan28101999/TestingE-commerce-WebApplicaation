f84075f9fcc88a3e48b9cf9735f7af825e19dc2a
karthi <karthikeyan28mk@gmail.com>
2022-08-15 05:48:32 +0530

    second commit

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -24,18 +26,28 @@ public class Base {
 		propertyFile=new Properties();
 		FileInputStream file=new FileInputStream(projectPath+"\\resource\\userdata.properties");
 		propertyFile.load(file);
-		String brwsrName=propertyFile.getProperty("browser");
+		String brwsrName=System.getProperty("browser");
 		
 		//condition for which browser to initialized.
-		if(brwsrName.equals("chrome"))
+		if(brwsrName.contains("chrome"))
 		{
-			WebDriverManager.chromedriver().setup();
-		    driver=new ChromeDriver();
+		    WebDriverManager.chromedriver().setup();
+                     ChromeOptions option=new ChromeOptions();
+                     if(brwsrName.equals("chromeheadless"))
+                     {
+                       option.addArguments("headless");
+                     }
+		    driver=new ChromeDriver(option);
 		}
-		else if(brwsrName.equals("firefox"))
+		else if(brwsrName.contains("firefox"))
 		{
 			WebDriverManager.firefoxdriver().setup();
-			driver=new FirefoxDriver();
+                        FirefoxOptions option=new FirefoxOptions();
+                        if(brwsrName.equals("firefoxheadless"))
+                        {
+                          option.addArguments("-headless");
+                         }
+			driver=new FirefoxDriver(option);
 		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
